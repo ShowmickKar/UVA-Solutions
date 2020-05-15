c7516aab5893f30cda359383805a0a3d887e1099
Showmick Kar <showmick12@gmail.com>
2020-05-15 11:56:43 +0600

    Create 11044 - Searching for Nessy.cpp

---
+++ src/11044 - Searching for Nessy.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	int n, m; cin >> n >> m;
+	cout << (n/3) * (m/3) << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+ 
+	int t; cin >> t;
+	while (t--)  
+		solve();
+}
+
