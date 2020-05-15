0341579aabfe8cae530800111c86d6af76d4af4b
Showmick Kar <showmick12@gmail.com>
2020-05-15 10:32:33 +0600

    Create 1124 Celebrity jeopardy.cpp

---
+++ src/1124 Celebrity jeopardy.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	string s;
+	while (getline(cin, s)) {
+		cout << s << endl;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+ 
+	/* int t; cin >> t;
+	while (t--)  */
+		solve();
+}
+
