a7064383325ef5e04f3877c1312bc2a6b068e313
Showmick Kar <showmick12@gmail.com>
2020-05-22 16:05:12 +0600

    Create 458 - The Decoder.cpp

---
+++ src/458 - The Decoder.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	char c;
+	while(scanf("%c", &c) != EOF) {
+		if (c == '\n') printf("\n");
+		else printf("%c", c - 7);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+ 
+	/* int t; scanf("%d", &t);
+	while(t--) */ 
+		solve();
+}
